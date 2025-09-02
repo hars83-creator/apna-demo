2290f211b848d17c63ed60f0c77de34c7aebbe52
Harsh <harsh97830544@gmail.com>
2025-09-02 11:55:12 +0000

    Add initial file for calculator program

---
+++ calculator.c
@@ -0,0 +1,67 @@
+//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+/*
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+#include <stdio.h>
+
+int main() {
+    char operator;
+    int num1, num2, result;
+
+    printf("Enter an operator : ");
+    scanf(" %c", &operator);
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, result);
+            } else {
+                printf("Division by zero error!\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                result = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, result);
+            } else {
+                printf("Modulo by zero error!\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
