6dce83f8d1280cddc2e9cf7a9411ac4244a9b613
Harsh <harsh97830544@gmail.com>
2025-10-12 03:04:20 +0000

    // Q100: Print all sub-strings of a string.

---
+++ que100.c
@@ -0,0 +1,31 @@
+// Q100: Print all sub-strings of a string.
+
+/*
+Sample Test Cases:
+Input 1:
+abc
+Output 1:
+a,ab,abc,b,bc,c
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[101];
+    printf("Enter a string: ");
+    scanf("%s", s);
+    int len = strlen(s);
+    int first = 1;
+    for (int i = 0; i < len; i++) {
+    for (int j = i; j < len; j++) {
+            
+    if (!first) printf(",");
+    for (int k = i; k <= j; k++)
+    putchar(s[k]);
+    first = 0;
+    }
+    }
+    printf("\n");
+    return 0;
+}
